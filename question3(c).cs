5acbd7dda193320b9a94df130d24979b954da2dc
BeratAltunn <beratalltun8@gmail.com>
2025-03-08 02:51:05 +0300

    Data Structures Project 3

---
+++ question3(c).cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public class UM_Alanı
+    {
+        public string Alan_Adı { get; set; }
+        public List<string> İl_Adları { get; set; }
+        public int İlan_Yılı { get; set; }
+    }
+
+    static void Main()
+    {
+        SortedSet<UM_Alanı> alanHeap = new SortedSet<UM_Alanı>(Comparer<UM_Alanı>.Create((x, y) => string.Compare(x.Alan_Adı, y.Alan_Adı, StringComparison.Ordinal)));
+
+        string[] mirasAlanları =  {
+            "Divriği Ulu Camii ve Darüşşifası,Sivas,1985",
+            "İstanbul'un Tarihi Alanları,İstanbul,1985",
+            "Göreme Millî Parkı ve Kapadokya,Nevşehir,1985",
+            "Hattuşa: Hitit Başkenti,Çorum,1986",
+            "Nemrut Dağı,Adıyaman,1987",
+            "Hieropolis-Pamukkale,Denizli,1988",
+            "Xanthos-Letoon,Antalya-Muğla,1988",
+            "Safranbolu Şehri,Karabük,1994",
+            "Truva Arkeolojik Alanı,Çanakkale,1998",
+            "Edirne Selimiye Camii ve Külliyesi,Edirne,2011",
+            "Çatalhöyük Neolitik Alanı,Konya,2012",
+            "Bursa ve Cumalıkızık: Osmanlı İmparatorluğunun Doğuşu,Bursa,2014",
+            "Bergama Çok Katmanlı Kültürel Peyzaj Alanı,İzmir,2014",
+            "Diyarbakır Kalesi ve Hevsel Bahçeleri Kültürel Peyzajı,Diyarbakır,2015",
+            "Efes,İzmir,2015",
+            "Ani Arkeolojik Alanı,Kars,2016",
+            "Aphrodisias,Aydın,2017",
+            "Göbekli Tepe,Şanlıurfa,2018",
+            "Arslantepe Höyüğü,Malatya,2021",
+            "Gordion,Ankara,2023",
+            "Anadolu’nun Ortaçağ Dönemi Ahşap Hipostil Camiileri,(Konya-Eşrefoğlu Camii- Kastamonu-Mahmut Bey Camii- Eskişehir-Sivrihisar Camii- Afyon-Afyon Ulu Camii- Ankara-Arslanhane Camii),2023"
+
+        };
+
+        foreach (var alanVerisi in mirasAlanları)
+        {
+            string[] alanBilgileri = alanVerisi.Split(',');
+            UM_Alanı umAlan = new UM_Alanı
+            {
+                Alan_Adı = alanBilgileri[0],
+                İl_Adları = new List<string> { alanBilgileri[1] },
+                İlan_Yılı = int.Parse(alanBilgileri[2])
+            };
+
+            alanHeap.Add(umAlan);
+        }
+
+        TumAlanlariYazdir(alanHeap);
+    }
+
+    static void TumAlanlariYazdir(SortedSet<UM_Alanı> alanHeap)
+    {
+
+        var ilkUcAlan = alanHeap.Take(3);
+
+        foreach (var alan in ilkUcAlan)
+        {
+            Console.WriteLine($"Alan Adı: {alan.Alan_Adı} - İller: {string.Join(", ", alan.İl_Adları)} - İlan Yılı: {alan.İlan_Yılı}");
+        }
+    }
+}
---
+++ question4.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace SortingAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] sampleArray = { 5, 2, 9, 1, 5, 6 };
+
+            
+            ISorter bubbleSorter = new Bubble();
+            Console.WriteLine("Bubble Sort Örneği:");
+            Console.WriteLine("Orijinal Dizi: " + ArrayToString(sampleArray));
+            bubbleSorter.Execute(sampleArray);
+            Console.WriteLine("Sıralı Dizi: " + ArrayToString(sampleArray));
+            Console.WriteLine();
+
+            ISorter quickSorter = new Quick();
+            Console.WriteLine("Quick Sort Örneği:");
+            Console.WriteLine("Orijinal Dizi: " + ArrayToString(sampleArray));
+            quickSorter.Execute(sampleArray);
+            Console.WriteLine("Sıralı Dizi: " + ArrayToString(sampleArray));
+
+            Console.ReadLine();
+        }
+
+        static string ArrayToString(int[] array)
+        {
+            return "[" + string.Join(", ", array) + "]";
+        }
+    }
+
+    
+    public interface ISorter
+    {
+        string Description { get; }
+        void Execute(int[] array);
+    }
+
+
+    public class Bubble : ISorter
+    {
+        public string Description => "Bubble Sort Sıralama Algoritması";
+
+        public void Execute(int[] array)
+        {
+            int n = array.Length;
+            int temp;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                     
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    public class Quick : ISorter
+    {
+        public string Description => "Quick Sort Sıralama Algoritması";
+
+        public void Execute(int[] array)
+        {
+            Sort(0, array.Length - 1, array);
+        }
+
+        private void Sort(int leftValue, int rightValue, int[] array)
+        {
+            if (leftValue < rightValue)
+            {
+                int pivotIndex = Partition(leftValue, rightValue, array);
+
+                Sort(leftValue, pivotIndex - 1, array);
+                Sort(pivotIndex + 1, rightValue, array);
+            }
+        }
+
+        private int Partition(int leftValue, int rightValue, int[] array)
+        {
+            int pivotValue = array[rightValue];
+            int i = leftValue - 1;
+
+            for (int j = leftValue; j < rightValue; j++)
+            {
+                if (array[j] < pivotValue)
+                {
+                    i++;
+
+                   
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+
+     
+            int tempSwap = array[i + 1];
+            array[i + 1] = array[rightValue];
+            array[rightValue] = tempSwap;
+
+            return i + 1;
+        }
+    }
+}
